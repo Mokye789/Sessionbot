b619dabc919e8c75ac3ffbd0b62707187c0662d9
Mokye789 <127610392+Mokye789@users.noreply.github.com>
2023-08-03 22:03:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://shadow:BE_XB@cluster0.noovm8n.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 6199134030))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/spzohary")
