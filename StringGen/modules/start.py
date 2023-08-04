dd61ff1842d0872cfa69252c4ab1a43294e89f62
Mokye789 <127610392+Mokye789@users.noreply.github.com>
2023-08-04 03:29:34 +0300

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,10 +8,10 @@ ch = "spzohary"
 
 @Anony.on_message(filters.private)
 async def me(client, message):
-  if message.from_user.id not in [6199134030,6392010766,5839884860,5552799584]:
    try:
       await Anony.get_chat_member(ch, message.from_user.id)
    except:
+     if message.from_user.id not in [6199134030,6392010766,5839884860,5552799584]:
       return await message.reply_text(f"يجب ان تشترك ف قناة السورس أولا \n https://t.me/{ch}")
    message.continue_propagation()
 
